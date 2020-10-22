37ab9f19ca65d0bc6a5820632d6bec4838be6cdc
Yaroslav Skachkov <ikanom96@gmail.com>
2020-10-22 13:06:30 +0300

    CitiesVC added.

---
+++ WeatherApp/CitiesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CitiesViewController.swift
+//  WeatherApp
+//
+//  Created by Ярослав on 10/20/20.
+//
+
+import UIKit
+
+class CitiesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.hidesBackButton = true
+    
+    }
+}
+
